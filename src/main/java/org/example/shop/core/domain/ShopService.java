b4ad496465a7337aaa1f69f6d297d9d516e0247c
jsween5723 <jsween5723@gmail.com>
2024-07-19 10:44:33 +0900

    feat(shop): 비즈니스 로직 정의

---
+++ src/main/java/org/example/shop/core/domain/ShopService.java
@@ -0,0 +1,25 @@
+package org.example.shop.core.domain;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShopService {
+    public void create(CreateShop createShop) {
+
+    }
+
+    public void modify(ModifyShop modifyShop) {
+
+    }
+    public void delete(long shopId) {
+
+    }
+    public Shop getById(long id) {
+        return null;
+    }
+
+    public List<Shop> findAll() {
+        return null;
+    }
+}
