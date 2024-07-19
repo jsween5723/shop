0bc3bc6fe377ac38146aedfeb667383cd936e962
jsween5723 <jsween5723@gmail.com>
2024-07-19 12:05:11 +0900

    feat(menu): 비즈니스 로직 정의

---
+++ src/main/java/org/example/shop/core/domain/menu/MenuService.java
@@ -0,0 +1,27 @@
+package org.example.shop.core.domain.menu;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MenuService {
+
+    public void create(CreateMenu createMenu) {
+
+    }
+
+    public void delete(long menuId) {
+    }
+
+    public void modify(long menuId, ModifyMenu modifyMenu) {
+    }
+
+    public Menu findById(long id) {
+        return null;
+    }
+
+    public List<Menu> findAll() {
+        return null;
+    }
+
+}
