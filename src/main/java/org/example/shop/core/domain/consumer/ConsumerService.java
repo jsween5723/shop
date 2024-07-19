312b8d9c60178f03398068d0930893e6371ffa24
jsween5723 <jsween5723@gmail.com>
2024-07-19 12:21:42 +0900

    feat(consumer): 비즈니스 로직 선언

---
+++ src/main/java/org/example/shop/core/domain/consumer/ConsumerService.java
@@ -0,0 +1,28 @@
+package org.example.shop.core.domain.consumer;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConsumerService {
+
+    public void create(CreateConsumer consumer) {
+
+    }
+
+    public void delete(long id) {
+
+    }
+
+    public void modify(long id, ModifyConsumer consumer) {
+
+    }
+
+    public Consumer findById(long id) {
+        return null;
+    }
+
+    public List<Consumer> findAll() {
+        return null;
+    }
+}
