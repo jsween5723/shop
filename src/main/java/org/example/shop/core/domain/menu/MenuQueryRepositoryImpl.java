30c2679ba2d2cc897b304e1aafe3f3d188ecc18e
jsween5723 <jsween5723@gmail.com>
2024-07-20 16:43:37 +0900

    fix(menu): 매출 조회 쿼리, 인기순 정렬 쿼리 수정

--- src/main/java/org/example/shop/core/domain/menu/MenuQueryRepositoryImpl.java
+++ src/main/java/org/example/shop/core/domain/menu/MenuQueryRepositoryImpl.java
@@ -18,9 +18,9 @@ public class MenuQueryRepositoryImpl implements MenuQueryRepository {
 
     @Override
     public List<MenuEntity> findByQuery(FindMenuQuery query) {
-        String sql = "select m from menus m";
+        String sql = "select m from menus m ";
         if (isExistCategory(query)) {
-            sql += "where m.categoty = :category";
+            sql += "where m.category = :category";
         }
 
         TypedQuery<MenuEntity> statement = entityManager.createQuery(sql, MenuEntity.class);
@@ -32,9 +32,9 @@ public class MenuQueryRepositoryImpl implements MenuQueryRepository {
 
     @Override
     public List<RankMenu> rank3ByOrderCount() {
-        String sql = "select m.id, m.category, m.name, m.price, m.description, count(m) as totalOrderNumber "
-            + "from order_items oi join menus m "
-            + "group by m.id order by totalOrderNumber DESC limit 3";
+        String sql = "select new org.example.shop.core.domain.menu.RankMenu(m.id, m.category, m.name, m.description, m.price, cast(count(oi) as int)) "
+            + "from order_items oi right join menus m on oi.menu.id = m.id "
+            + "group by m.id order by count(m) DESC limit 3";
         return entityManager.createQuery(sql, RankMenu.class).getResultList();
     }
 
